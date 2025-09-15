edc247bfb540c70af0edc3bfb1a09c81ce4e2407
Martino <martino.fabbro@gmail.com>
2025-09-15 08:03:33 -0700

    Fix client-side session not getting organization data
    - Remove hardcoded null session from AuthProvider
    - Let SessionProvider fetch session automatically
    - This fixes the issue where organizationSlug wasn't showing in UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider session={null}>
+        <AuthProvider>
           {children}
         </AuthProvider>
         <Toaster />
